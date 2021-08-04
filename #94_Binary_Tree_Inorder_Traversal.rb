5ee2975d84c4832327e17dc4783161012f51b211
npakk <npakk669@gmail.com>
2021-08-04 15:02:45 +0900

    #94

---
+++ #94_Binary_Tree_Inorder_Traversal.rb
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def inorder_traversal(root)
+  return [] if root.nil?
+
+  inorder_traversal(root.left) + [root.val] + inorder_traversal(root.right)
+end
